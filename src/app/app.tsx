a688068df2802bf9c470fa1adf92f81ef1d5bdd3
NP <lavish@NJays-MacBook-Pro.local>
2023-01-03 20:27:48 -0500

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="well-rounded-pop" />
+      <div />
+    </>
+  );
+}
+
+export default App;
